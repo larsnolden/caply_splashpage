9f19e39849d71733fa2b5327a7c61edef09a2504
Lars Nolden <lars@larsnolden.com>
2019-01-09 07:52:43 -0500

    team cat. styling

--- src/views/Splashpage/TheTeam.js
+++ src/views/Splashpage/TheTeam.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import styles from 'style_variables';
 const TeamPictureUrl = '/media/team.png';
 
 const Wrapper = styled.div`
-  width: 100%;
+  max-width: 1920px;
+  margin: 109px auto;
+  @media screen and (max-width: 1300px) {
+    margin-top: 36px;
+  }
+`;
+
+const Container = styled.div`
+  min-height: 800px;
   display: flex;
   flex-direction: row;
-  height: 100vh;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
 const FeatureHeading = styled.div`
@@ -19,7 +27,6 @@ const FeatureHeading = styled.div`
   font-size: 36px;
   letter-spacing: 0.05em;
   color: #2E2E2E;
-  margin-left: 36px;
 `;
 
 const FeatureText = styled.div`
@@ -33,34 +40,51 @@ const FeatureText = styled.div`
   margin-top: 14px;
 `;
 
+const HalfContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-basis: 50%;
+  @media screen and (max-width: 1300px) {
+    flex-basis: 100%
+  }
+`;
+
 const TextContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-basis: 50%;
+  flex-direction: column;
+  justify-content: flex-start;
+  max-width: 640px;
+  @media screen and (max-width: 1300px) {
+    margin-top: 36px;
+  }
 `;
 
-const TeamPictureContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-basis: 50%;
+const TeamPicture = styled.img`
+  width: 100%;
+  max-width: 544px;
+  display: block;
+  @media screen and (max-width: 1300px) {
+    margin-top: 36px;
+  }
 `;
-
-const TeamPicture = styled.img``;
 
 export default () =>
-  <Wrapper>
-    <TextContainer>
-      <FeatureHeading>
-        The Team
-      </FeatureHeading>
-      <FeatureText>
-        We are a team of engineers and friends that recognized the need for automation to succeed with Amazon PPC on a large scale.
-      </FeatureText>
-    </TextContainer>
-    <TeamPictureContainer>
+<Wrapper>
+  <Container>
+    <HalfContainer>
+      <TextContainer>
+        <FeatureHeading>
+          The Team
+        </FeatureHeading>
+        <FeatureText>
+          We are a team of engineers and friends that recognized the need for automation to succeed with Amazon PPC on a large scale.
+        </FeatureText>
+      </TextContainer>
+    </HalfContainer>
+    <HalfContainer>
         <TeamPicture src={TeamPictureUrl}/>
-    </TeamPictureContainer>
-
-  </Wrapper>
+    </HalfContainer>
+  </Container>
+</Wrapper>
+;
